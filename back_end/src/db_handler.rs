74ca3fd6966e7a108360b4d50d705c9959f8f195
Stephen Seo <seo.disparate@gmail.com>
2022-04-06 20:49:54 +0900

    Add GPL3 Licence headers to sources

--- back_end/src/db_handler.rs
+++ back_end/src/db_handler.rs
@@ -1,3 +1,11 @@
+//Four Line Dropper Backend - A server enabling networked multiplayer for Four Line Dropper
+//Copyright (C) 2022 Stephen Seo
+//
+//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 use crate::ai::{get_ai_choice, AIDifficulty};
 use crate::constants::{
     COLS, GAME_CLEANUP_TIMEOUT, PLAYER_CLEANUP_TIMEOUT, PLAYER_COUNT_LIMIT, ROWS, TURN_SECONDS,
--- front_end/src/state.rs
+++ front_end/src/state.rs
@@ -1,3 +1,11 @@
+//Four Line Dropper Frontend/Backend - A webapp that allows one to play a game of Four Line Dropper
+//Copyright (C) 2022 Stephen Seo
+//
+//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 use crate::ai::AIDifficulty;
 use crate::constants::{COLS, ROWS};
 use crate::game_logic::{check_win_draw, WinType};
